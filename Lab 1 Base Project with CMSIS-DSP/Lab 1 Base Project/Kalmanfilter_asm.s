19a5667c49bde0816ba4f849e0750febcc73ebcc
chiwingsit <chiwingsit@gmail.com>
2015-02-03 12:28:02 -0500

    Simplified assembly code

--- Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/Kalmanfilter_asm.s
+++ Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/Kalmanfilter_asm.s
@@ -11,8 +11,6 @@
 	; S2: value of x
 	; S3: value of p
 	; S4: value of k
-	; S5: value of next measurement from input
-	; S6: temp
 	
 test_workbench
 
@@ -49,61 +47,59 @@ Kalmanfilter_asm
 	 
 	;LOOP =====================================
 loop
+	;p = p + q
+	VADD.F32 S3, S3, S0
 
-	 ;load next measurement from input array
-	 VLDR.32 S5, [R0]
-	 ;increment input array index
-	 ADD R0, R0, #4
+	;temp = p + r
+	VADD.F32 S5, S3, S1
+	;k = p / p + r
+	VDIV.F32 S4, S3, S5
 
-	 ;p = p + q
-	 VADD.F32 S3, S3, S0
-	 
-	 ;temp = p + r
-	 VADD.F32 S6, S3, S1
-	 ;k = p / p + r
-	 VDIV.F32 S4, S3, S6
+	;load next measurement from input array
+	VLDR.32 S5, [R0]
+	;increment input array index
+	ADD R0, R0, #4
 
-	 ;temp = measurement - x
-	 VSUB.F32 S6, S5, S2
-	 ;temp = k * (measurement - x)
-	 VMUL.F32 S6, S4, S6
-	 ;x = x + k * (measurement - x)
-	 VADD.F32 S2, S2, S6
-	 
-	 ;temp = 1 - k
-	 VLDR.F32 S7, =1.0
-	 VSUB.F32 S6, S7, S4
-	 ;p = (1 - k) * p
-	 VMUL.F32 S3, S6, S3
-	 
-	 ;store x value to output array
-	 VSTR.32 S2, [R1]
-	 ;increment output array index
-	 ADD R1, R1, #4
-	 
-	 ;decrement array length (# of elements remaining)
-	 SUBS R3, R3, #1
-	 ;if end of array not reached, loop
-	 BNE  loop
-	 
-	 ;LOOP END=================================
-	 
-	 ;STORE PARAMETERS TO STRUCT ==============
-	 ;store q
-	 VSTR.32 S0, [R2]
-	 ;store r
-	 VSTR.32 S1, [R2,#4]
-	 ;store x
-	 VSTR.32 S2, [R4,#8]
-	 ;store p
-	 VSTR.32 S3, [R4,#12]
-	 ;store k
-	 VSTR.32 S4, [R4,#16]
-	 
-	 BX LR
-	 
-	 END
-	 
-	 
-	 
-	  
+	;temp = measurement - x
+	VSUB.F32 S5, S5, S2
+	;temp = k * (measurement - x)
+	VMUL.F32 S5, S4, S5
+	;x = x + k * (measurement - x)
+	VADD.F32 S2, S2, S5
+
+	;temp = 1 - k
+	VLDR.F32 S5, =1.0
+	VSUB.F32 S5, S5, S4
+	;p = (1 - k) * p
+	VMUL.F32 S3, S5, S3
+
+	;store x value to output array
+	VSTR.32 S2, [R1]
+	;increment output array index
+	ADD R1, R1, #4
+
+	;decrement array length (# of elements remaining)
+	SUBS R3, R3, #1
+	;if end of array not reached, loop
+	BNE  loop
+
+	;LOOP END=================================
+
+	;STORE PARAMETERS TO STRUCT ==============
+	;store q
+	VSTR.32 S0, [R2]
+	;store r
+	VSTR.32 S1, [R2,#4]
+	;store x
+	VSTR.32 S2, [R4,#8]
+	;store p
+	VSTR.32 S3, [R4,#12]
+	;store k
+	VSTR.32 S4, [R4,#16]
+
+	BX LR
+
+	END
+
+
+
