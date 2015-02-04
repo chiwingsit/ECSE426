f6577880d5407131b0bcdd90e70b5d23ea1ad783
chiwingsit <chiwingsit@gmail.com>
2015-02-03 22:23:32 -0500

    Added Lab 2 Part 3

--- Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/source/main.c
+++ Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/source/main.c
@@ -15,6 +15,66 @@ typedef struct
 
 extern int Kalmanfilter_asm (float* InputArray, float* OutputArray, kalman_state* kstate, int length);
 
+float* subs_diff(float* inputArray, float* outputArray, int n){
+	int i;
+	float subArray[10];
+	for (i = 0; i < n; i++)
+	{	
+		/*substraction of original and output */
+		subArray[i] = inputArray[i] - outputArray[i];	
+	}
+	return subArray;
+}
+
+float average_diff(float* x, int n){
+	int i;
+	float sum=0, average;
+	for (i = 0; i < n; i++)
+	{
+			sum = sum + x[i];
+	}
+	average = sum / (float)n;
+	return average;
+		
+}
+
+float standard_deviation(float* data, int n){
+	float mean=0.0, sum_deviation=0.0;
+	int i;
+	for(i=0; i<n;++i)
+	{
+		mean+=data[i];
+	}
+	mean=mean/n;
+	for(i=0; i<n;++i)
+	sum_deviation+=(data[i]-mean)*(data[i]-mean);
+	return sqrt(sum_deviation/n); 
+				
+}
+float* correlation(float* inputArray, float* outputArray, int n){
+	float r[n];
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		r[i] = inputArray[i] * outputArray[n-i];
+	}
+	return r;
+}
+
+float* convolution(float* inputArray, float* outputArray, int n){
+	float r[n];
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		r[i] = inputArray[i] * outputArray[i];
+	}
+	return r;
+}
+
+//  arm_correlate_f32 (float32_t *pSrcA, uint32_t srcALen, float32_t *pSrcB, uint32_t srcBLen, float32_t *pDst);
+//	arm_conv_f32 (	float32_t * 	pSrcA,uint32_t 	srcALen,float32_t * 	pSrcB,uint32_t 	srcBLen,float32_t * 	pDst )		
+		
+
 int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int length)
 {
 	int i;
@@ -54,7 +114,30 @@ int main()
 	kstate->x = 0.0;
 	
 	Kalmanfilter_C(pIn,pOut,kstate,10);
+	float *pointer_diff_C;
+	float *pointer_diff_CMSIS;
 	
+	pointer_diff_C = subs_diff(pIn, pOut, 10);
+	arm_sub_f32 ( pIn, pOut, pointer_diff_CMSIS, 10);
+	
+	float mean_C;
+	float32_t mean_CMSIS;
+	mean_C = average_diff(pointer_diff_C, 10);
+	arm_mean_f32(pOut, 10, &mean_CMSIS);
+	
+	float std_C;
+	float32_t std_CMSIS;
+	std_C = standard_deviation(pointer_diff_C, 10);
+	arm_std_f32(pointer_diff_CMSIS, 10, &std_CMSIS);
+	
+	float32_t *pDst;
+	correlation(pIn,	pOut, 10);
+	arm_correlate_f32 (pIn, 10, pOut, 10, pDst);
+	
+	convolution(pIn, pOut,10);
+	arm_conv_f32 (pIn, 10, pOut, 10, pDst);	
+	
+	while(1);
 	return 0;
 }
 
