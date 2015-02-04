c908b6855fee888332cf8d3c0212fb34264fa06a
unknown <kwang26@156TR4160MP-22.campus.MCGILL.CA>
2015-02-04 16:01:19 -0500

    Add complete assembly test workbench and outputs

---
+++ Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/output.s
@@ -0,0 +1,8 @@
+	AREA results, DATA, READWRITE
+	EXPORT output
+	EXPORT struct
+		
+output	SPACE 600		;create emtpy output array
+struct	SPACE 20		;create empty struct
+	
+	END
--- Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/test_workbench.s
+++ Lab 1 Base Project with CMSIS-DSP/Lab 1 Base Project/test_workbench.s
@@ -1,19 +1,19 @@
 	AREA text, CODE, READONLY
 	IMPORT Kalmanfilter_asm
+	IMPORT output
+	IMPORT struct
 	EXPORT test_workbench
 
-;create arrays
-input	DCFS	1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0
-output	SPACE	600
-struct	SPACE	20	;create struct
+input	DCFS	0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0 	;create input array
 
 test_workbench
 
 	;INITIALIZE PARAMETERS ==============
 	ADR R0, input	;set pointer to input arrays
-	ADR R1, output	;set pointer to output arrays
-	ADR R2, struct	;set pointer to struct
+	LDR R1, =output	;set pointer to output arrays
+	LDR R2, =struct	;set pointer to struct
 	LDR R3, =10 	;set array length
+	LDR R5, =output
 
 	VLDR.F32 S0, =0.1
 	VSTR.32 S0, [R2]	;initialise q
