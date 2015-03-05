fc929a7a5a2cd0a2935ed1fce772ec8144878a26
chiwingsit <chiwingsit@gmail.com>
2015-03-05 12:41:29 -0500

    Update main.c

--- Lab 3 Base Project/src/main.c
+++ Lab 3 Base Project/src/main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "stm32f4xx.h"                  // Device header
 #include "stm32f4xx_conf.h"
-#include "ACC.h"
 
 int main(){
 	
-	ACC_enable();
+	while(1){
+	}
 	
 	return 0;
 }
-
 
