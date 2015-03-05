69001ea71cca42ca3483d68a981a4772ff142bb3
chiwingsit <chiwingsit@gmail.com>
2015-03-05 12:37:51 -0500

    Remove unused files

--- Lab 3 Base Project/src/ACC.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACC_h
-#define ACC_h
-
-#include <stdio.h>
-#include "lis3dsh.h"
-
-void ACC_enable ();
-
-#endif
--- Lab 3 Base Project/src/accelerometer.h
+++
@@ -1,6 +0,0 @@
-#ifndef acceleometer_h
-#define acceleometer_h
-
-#include "lis3dsh.h"
-
-#endif
